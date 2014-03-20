e7c1a7674585b3d7e6f434217fe0476611d11a94
Geert-Johan Riemer <geertjohan.riemer@gmail.com>
2014-03-20 13:58:04 +0100

    Change exec example

--- simpel/exec.go
+++ simpel/exec.go
@@ -4,15 +4,32 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 )
 
 func main() {
 	// START OMIT
-	cmd := exec.Command("git", "log", "-5", "--pretty=format:%h%x09%an%x09%s")
-	cmd.Dir = "/home/geertjohan/.oh-my-zsh"
-	out, err := cmd.CombinedOutput()
-	fmt.Println(err)
-	fmt.Println(string(out))
+	findDir := os.Getenv("HOME") + "/Workspaces/Go/devpath/src/github.com/GeertJohan"
+	cmdFind := exec.Command("find", ".", "-name", "*.go")
+	cmdFind.Dir = findDir
+
+	cmdWc := exec.Command("xargs", "wc", "-l")
+	cmdWc.Dir = findDir
+	cmdWc.Stdin, _ = cmdFind.StdoutPipe()
+	cmdWc.Stdout = os.Stdout
+	cmdWc.Start() // start wc (don't wait for it to finish)
+
+	err := cmdFind.Run() // start find, and wait for it to finish
+	if err != nil {
+		fmt.Printf("Error finding *.go files: %s\n", err)
+		return
+	}
+
+	err = cmdWc.Wait() // wait for wc to finish
+	if err != nil {
+		fmt.Printf("Error wc-ing *.go files: %s\n", err)
+		return
+	}
 	// END OMIT
 }
